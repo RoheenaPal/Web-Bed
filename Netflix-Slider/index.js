762707201a750b724b7679d9a2fae66f73c30de8
Roheena Pal <98142570+RoheenaPal@users.noreply.github.com>
2022-02-11 11:25:10 +0530

    Add files via upload

---
+++ Netflix-Slider/index.js
@@ -0,0 +1,52 @@
+const sliders=document.querySelector(".carouselbox")
+var scrollPerClick;
+var ImagePadding=20
+
+showMovieData()
+
+var scrollAmount=0;
+
+function sliderScrollLeft(){
+    sliders.scrollTo({
+        top: 0,
+        left: (scrollAmount -= scrollPerClick),
+        behaviour: "smooth"
+    });
+
+    if(scrollAmount < 0){
+        scrollAmount=0
+    }
+}
+function sliderScrollRight() {
+    if(scrollAmount <= sliders.scrollWidth - sliders.clientWidth){
+        sliders.scrollTo({
+            top: 0,
+            left: (scrollAmount += scrollPerClick),
+            behaviour: "smooth",
+        })
+    }
+}
+
+
+
+
+async function showMovieData(){
+    const api_key="1268590fd0b518ebdddbeb4a3e70199c";
+
+    var resut = await axios.get(
+        "https://api.themoviedb.org/3/discover/movie?api_key="+
+        api_key +
+        "sort_by=popularity.desc"
+    ) ;
+
+    result = result.data.results
+
+    result.map(function (cur, index){
+        sliders.insertAdjacentHTML(
+            "beforeend"
+            `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${cur.poster_path}" />`
+        )
+    })
+
+    scrollPerClick = 400;
+}
